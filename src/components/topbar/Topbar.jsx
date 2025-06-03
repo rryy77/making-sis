41469645bb8896351bf3ec48944e406d43463e5a
rryy77 <ryusuke03062009@icloud.com>
2025-06-03 18:30:25 +0900

    html

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import { Chat, Notifications, Search } from "@mui/icons-material";
+import React from "react";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <div className="logo">Real SNS</div>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            type="text"
+            className="searchInput"
+            placeholder="探し物はなんですか？"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarIconItem">
+          <Chat />
+          <span className="topbarIconBadge">1</span>
+        </div>
+        <div className="topbarIconItem">
+          <Notifications />
+          <span className="topbarIconBadge">2</span>
+        </div>
+      </div>
+      <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+    </div>
+  );
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Topbar from "../../components/topbar/Topbar";
+
+export default function home() {
+  return (
+    <>
+      <Topbar />
+      {/*sidbar */}
+      {/* timeline */}
+      {/* righbar */}
+    </>
+  );
+}
