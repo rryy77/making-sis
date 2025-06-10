74abef49b8d444d9aae89dad4b97e4b16adf1ea7
rryy77 <ryusuke03062009@icloud.com>
2025-06-11 00:21:20 +0900

    main

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,5 +2,42 @@ import React from "react";
 import "./Rightbar.css";
 
 export default function Rightbar() {
-  return <div>Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="assets/star.png" alt="" className="starImg" />
+          <span className="eventText">
+            <b>Followers</b> only Event
+          </span>
+        </div>
+        <img src="assets/event.jpeg" alt="" className="eventImg" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/1.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">r-code</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/2.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">s-code</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
